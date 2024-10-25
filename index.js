7bf7a8f59aa7b473441b84cfca834b8d988221ac
Akhil Kommineni <67689764+akhilkommineni@users.noreply.github.com>
2024-10-25 17:35:21 +0530

    Update index.js
    Js structure fix

--- index.js
+++ index.js
@@ -19,34 +19,39 @@ let icons = {
   'scissors': 'https://img.icons8.com/ios-filled/50/scissors.png'
 }
 rulesHide.addEventListener('click', () => {
-  rulesContainer.classList.toggle("hide-container") middleContainer.classList.toggle(
-    "homepagecontainer-main-toggle")
-}) userChoices.forEach((eachEle) => {
+  rulesContainer.classList.toggle("hide-container");
+  middleContainer.classList.toggle("homepagecontainer-main-toggle");
+});
+userChoices.forEach((eachEle) => {
   eachEle.addEventListener('click', () => {
-    const userChoice = eachEle.getAttribute('value') userOption = userChoice
-    const computerChoice = getComputerChoice() const winner = determineWinner(userChoice, computerChoice) if (
-      winner === 'player') {
+    const userChoice = eachEle.getAttribute('value');
+    userOption = userChoice;
+    const computerChoice = getComputerChoice(); 
+    const winner = determineWinner(userChoice, computerChoice);
+    if (winner === 'player') {
       playerScore++;
       playerScoreEl.textContent = playerScore;
       homePageElement.classList.toggle('hide-container', true);
       computerWonElement.classList.toggle('hide-container', true);
       TieElement.classList.toggle('hide-container', true);
-      playerWonElement.classList.toggle('hide-container', false) playerWonElement.querySelector('.tryagain')
-        .addEventListener('click', () => {
+      playerWonElement.classList.toggle('hide-container', false); 
+      playerWonElement.querySelector('.tryagain').addEventListener('click', () => {
           reseteverything()
-        }) playerWonElement.querySelector("#player-icon").src = icons[userOption] playerWonElement
-        .querySelector("#comp-icon").src = icons[computerOption]
+        });
+      playerWonElement.querySelector("#player-icon").src = icons[userOption];
+      playerWonElement.querySelector("#comp-icon").src = icons[computerOption];
     } else if (winner === 'computer') {
       computerScore++;
       computerScoreEl.textContent = computerScore;
       homePageElement.classList.toggle('hide-container', true);
       computerWonElement.classList.toggle('hide-container', false);
       TieElement.classList.toggle('hide-container', true);
-      playerWonElement.classList.toggle('hide-container', true) computerWonElement.querySelector('.tryagain')
-        .addEventListener('click', () => {
-          reseteverything()
-        }) computerWonElement.querySelector("#player-icon").src = icons[userOption] computerWonElement
-        .querySelector("#comp-icon").src = icons[computerOption]
+      playerWonElement.classList.toggle('hide-container', true);
+      computerWonElement.querySelector('.tryagain').addEventListener('click', () => {
+          reseteverything();
+        }); 
+      computerWonElement.querySelector("#player-icon").src = icons[userOption];
+      computerWonElement.querySelector("#comp-icon").src = icons[computerOption];
     } else if (winner === 'draw') {
       computerScore++;
       computerScoreEl.textContent = computerScore;
@@ -55,16 +60,19 @@ rulesHide.addEventListener('click', () => {
       homePageElement.classList.toggle('hide-container', true);
       computerWonElement.classList.toggle('hide-container', true);
       TieElement.classList.toggle('hide-container', false);
-      playerWonElement.classList.toggle('hide-container', true) TieElement.querySelector('.tryagain')
-        .addEventListener('click', () => {
+      playerWonElement.classList.toggle('hide-container', true);
+      TieElement.querySelector('.tryagain').addEventListener('click', () => {
           reseteverything()
-        }) TieElement.querySelector("#player-icon").src = icons[userOption] TieElement.querySelector(
-          "#comp-icon").src = icons[computerOption] console.log(TieElement.querySelector("#comp-icon"))
+        });
+      TieElement.querySelector("#player-icon").src = icons[userOption]; 
+      TieElement.querySelector("#comp-icon").src = icons[computerOption];
     }
   })
-}) const choices = ['rock', 'paper', 'scissors'] rulesBtn.addEventListener('click', () => {
-  rulesContainer.classList.toggle("hide-container") middleContainer.classList.toggle(
-    "homepagecontainer-main-toggle")
+}) 
+const choices = ['rock', 'paper', 'scissors'];
+rulesBtn.addEventListener('click', () => {
+  rulesContainer.classList.toggle("hide-container");
+  middleContainer.classList.toggle("homepagecontainer-main-toggle");
 })
 
 function getComputerChoice() {
@@ -77,22 +85,21 @@ function determineWinner(player, computer) {
   if (player === computer) {
     return 'draw';
   }
-  if ((player === 'rock' && computer === 'scissors') || (player === 'paper' && computer === 'rock') || (player ===
-      'scissors' && computer === 'paper')) {
+  if ((player === 'rock' && computer === 'scissors') || (player === 'paper' && computer === 'rock') || (player ==='scissors' && computer === 'paper')) {
     return 'player';
   }
   return 'computer';
 }
 
 function reseteverything() {
-  homePageElement.classList.remove('hide-container') playerWonElement.classList.contains('hide-container') ? '' :
-    playerWonElement.classList.add('hide-container');
+  homePageElement.classList.remove('hide-container');
+  playerWonElement.classList.contains('hide-container') ? '' : playerWonElement.classList.add('hide-container');
   computerWonElement.classList.contains('hide-container') ? '' : computerWonElement.classList.add('hide-container');
   TieElement.classList.contains('hide-container') ? '' : TieElement.classList.add('hide-container');
-  playerWonElement.querySelector("#player-icon").img = ""
-  playerWonElement.querySelector("#comp-icon").img = ""
-  computerWonElement.querySelector("#player-icon").img = ""
-  computerWonElement.querySelector("#comp-icon").img = ""
-  TieElement.querySelector("#player-icon").img = ""
-  TieElement.querySelector("#comp-icon").img = ""
+  playerWonElement.querySelector("#player-icon").img = "";
+  playerWonElement.querySelector("#comp-icon").img = "";
+  computerWonElement.querySelector("#player-icon").img = "";
+  computerWonElement.querySelector("#comp-icon").img = "";
+  TieElement.querySelector("#player-icon").img = "";
+  TieElement.querySelector("#comp-icon").img = "";
 }
